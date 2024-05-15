2072d0cba2898624047e14e7e2e0fc3e63c5195c
Артур Захаркин <zaharkin.artur75@gmail.com>
2024-05-15 20:15:02 +0700

    Добавьте файлы проекта.

---
+++ Simulation Lab 12/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Simulation_Lab_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tbDays = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(43, 47);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(506, 244);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tbDays
+            // 
+            this.tbDays.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbDays.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbDays.Location = new System.Drawing.Point(129, 332);
+            this.tbDays.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbDays.Name = "tbDays";
+            this.tbDays.ReadOnly = true;
+            this.tbDays.Size = new System.Drawing.Size(195, 42);
+            this.tbDays.TabIndex = 1;
+            this.tbDays.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btStart.Location = new System.Drawing.Point(374, 317);
+            this.btStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(98, 28);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Запуск";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btStop.Location = new System.Drawing.Point(374, 361);
+            this.btStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(98, 28);
+            this.btStop.TabIndex = 3;
+            this.btStop.Text = "Стоп";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 416);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.tbDays);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox tbDays;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+    }
+}
+
---
+++ Simulation Lab 12/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_Lab_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double k = 0, t = 0;
+        int it = 0, flag = 0;
+        double[] count = new double[3];
+        double[,] pr = new double[3, 3];
+        string[] wet = new string[3];
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            wet[0] = "Ясно";
+            wet[1] = "Облачно";
+            wet[2] = "Пасмурно";
+
+            pr[0, 1] = 0.1; pr[0, 2] = 0.3;
+            pr[1, 0] = 0.3; pr[1, 2] = 0.1;
+            pr[2, 0] = 0.1; pr[2, 1] = 0.4;
+
+            for (int i = 0; i < 3; i++)
+            {
+                pr[i, i] = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i != j) pr[i, i] -= pr[i, j];
+                }
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                double tr = Math.Log(random.NextDouble()) / pr[it, it];
+                t += tr;
+                double[] p = new double[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    if (it != j) p[j] = -(pr[it, j] / pr[it, it]);
+                    else p[j] = 0;
+                }
+
+                double ran = random.NextDouble();
+                for (int j = 0; j < 3; j++)
+                {
+                    ran = ran - p[j];
+                    if (ran <= 0)
+                    {
+                        it = j;
+                        break;
+                    }
+                }
+            }
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int inew = 0;
+            double tn;
+
+            double tr = Math.Log(random.NextDouble()) / pr[it, it];
+            tn = t + tr;
+            k += tr;
+            double[] p = new double[3];
+            for (int j = 0; j < 3; j++)
+            {
+                if (it != j) p[j] = -(pr[it, j] / pr[it, it]);
+                else p[j] = 0;
+            }
+
+            double ran = random.NextDouble();
+            for (int j = 0; j < 3; j++)
+            {
+                ran = ran - p[j];
+                if (ran <= 0)
+                {
+                    inew = j;
+                    break;
+                }
+            }
+
+            count[it] += tn - t;
+            it = inew;
+            t = tn;
+
+            tbDays.Text = Math.Round(k, 3) + " дней";
+
+            if (flag == 1)
+            {
+                double sum = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    sum += count[i];
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    chart1.Series[0].Points.AddXY(wet[i], count[i] / sum);
+                }
+                timer1.Stop();
+            }
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+        }
+    }
+}
